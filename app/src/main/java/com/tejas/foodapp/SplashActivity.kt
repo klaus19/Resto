0d563b9fd4f014ff0e6d5cd1e28e15e434356fc6
Tejas <tejaskhartude@gmail.com>
2021-10-17 14:44:36 +0530

    Added all files

--- app/src/main/java/com/tejas/foodapp/SplashActivity.kt
+++ app/src/main/java/com/tejas/foodapp/SplashActivity.kt
@@ -44,7 +44,7 @@ class SplashActivity: AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
-        }, 3000)
+        }, 1000)
 
 
     }

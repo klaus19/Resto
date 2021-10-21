fbd0436a7af628a4e0e61a5aa9f0c1ec6bd91834
Tejas <tejaskhartude@gmail.com>
2021-10-21 21:17:05 +0530

    Changes have been made to AddUpdateDishActivity.kt while creating a CustomDialog

--- app/src/main/java/com/tejas/foodapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/tejas/foodapp/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,15 @@
 package com.tejas.foodapp.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.tejas.foodapp.R
 import com.tejas.foodapp.databinding.ActivityAddUpdateDishBinding
+import com.tejas.foodapp.databinding.DialogCustomImageSelectionBinding
 
-class AddUpdateDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity(),View.OnClickListener {
 
     // Create a global variable for binding
     private lateinit var mBinding: ActivityAddUpdateDishBinding
@@ -17,6 +21,8 @@ class AddUpdateDishActivity : AppCompatActivity() {
 
         setContentView(mBinding.root)
         setupActionBar()
+
+        mBinding.ivAddDishImage.setOnClickListener(this)
     }
 
     private fun setupActionBar() {
@@ -25,5 +31,37 @@ class AddUpdateDishActivity : AppCompatActivity() {
         mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
             onBackPressed()
         }
+    }
+
+    override fun onClick(v: View?) {
+        if(v!=null){
+            when(v.id){
+                R.id.iv_add_dish_image ->{
+                    customImageSelectionDialog()
+
+                    return
+                }
+            }
+        }
+    }
+
+    private fun customImageSelectionDialog(){
+        val dialog = Dialog(this)
+        val binding:DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener{
+            Toast.makeText(this,"Hello",Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener{
+            Toast.makeText(this,"Gallery",Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+
+
+        dialog.show()
     }
 }

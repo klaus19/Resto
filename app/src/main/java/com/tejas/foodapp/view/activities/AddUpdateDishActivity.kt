cdd0086c125fc4a648947b5c9adfe5ac05dfbe39
Tejas <tejaskhartude@gmail.com>
2021-10-21 19:32:19 +0530

    Added Toolbar to AddUpdateDishActivity.kt

--- app/src/main/java/com/tejas/foodapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/tejas/foodapp/view/activities/AddUpdateDishActivity.kt
@@ -3,10 +3,27 @@ package com.tejas.foodapp.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tejas.foodapp.R
+import com.tejas.foodapp.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    // Create a global variable for binding
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+
+        setContentView(mBinding.root)
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }

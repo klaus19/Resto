e28319fc7a93e7948034e413b9230025d28700c1
Tejas <tejaskhartude@gmail.com>
2021-10-23 19:53:02 +0530

    Changes made in AddUpdateDishActivity.kt

--- app/src/main/java/com/tejas/foodapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/tejas/foodapp/view/activities/AddUpdateDishActivity.kt
@@ -1,67 +1,184 @@
 package com.tejas.foodapp.view.activities
 
+import android.Manifest
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.tejas.foodapp.R
 import com.tejas.foodapp.databinding.ActivityAddUpdateDishBinding
 import com.tejas.foodapp.databinding.DialogCustomImageSelectionBinding
+import java.net.URI
 
 class AddUpdateDishActivity : AppCompatActivity(),View.OnClickListener {
 
-    // Create a global variable for binding
     private lateinit var mBinding: ActivityAddUpdateDishBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
 
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+
         setupActionBar()
 
-        mBinding.ivAddDishImage.setOnClickListener(this)
+        mBinding.ivAddDishImage.setOnClickListener(this@AddUpdateDishActivity)
     }
 
-    private fun setupActionBar() {
-        setSupportActionBar(mBinding.toolbarAddDishActivity)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
-            onBackPressed()
-        }
-    }
+    override fun onClick(v: View) {
 
-    override fun onClick(v: View?) {
-        if(v!=null){
-            when(v.id){
-                R.id.iv_add_dish_image ->{
-                    customImageSelectionDialog()
+        when (v.id) {
 
-                    return
-                }
+            R.id.iv_add_dish_image -> {
+
+                customImageSelectionDialog()
+                return
             }
         }
     }
 
-    private fun customImageSelectionDialog(){
-        val dialog = Dialog(this)
-        val binding:DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+    /**
+     * A function for ActionBar setup.
+     */
+    private fun setupActionBar() {
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+
+    /**
+     * A function to launch the custom image selection dialog.
+     */
+    private fun customImageSelectionDialog() {
+        val dialog = Dialog(this@AddUpdateDishActivity)
+
+        val binding: DialogCustomImageSelectionBinding =
+            DialogCustomImageSelectionBinding.inflate(layoutInflater)
+
+        /*Set the screen content from a layout resource.
+        The resource will be inflated, adding all top-level views to the screen.*/
         dialog.setContentView(binding.root)
 
-        binding.tvCamera.setOnClickListener{
-            Toast.makeText(this,"Hello",Toast.LENGTH_SHORT).show()
+        binding.tvCamera.setOnClickListener {
+
+            // TODO Step 3: Let ask for the permission while selecting the image from camera using Dexter Library. And Remove the toast message.
+            // START
+            Dexter.withContext(this@AddUpdateDishActivity)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        // Here after all the permission are granted launch the CAMERA to capture an image.
+                        if (report!!.areAllPermissionsGranted()) {
+
+                            // TODO Step 4: Show the Toast message for now just to know that we have the permission.
+                            // START
+                            Toast.makeText(
+                                this@AddUpdateDishActivity,
+                                "You have the Camera permission now to capture image.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            // END
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        // TODO Step 6: Show the alert dialog
+                        // START
+                        showRationalDialogForPermissions()
+                        // END
+                    }
+                }).onSameThread()
+                .check()
+            // END
+
             dialog.dismiss()
         }
 
-        binding.tvGallery.setOnClickListener{
-            Toast.makeText(this,"Gallery",Toast.LENGTH_SHORT).show()
+        binding.tvGallery.setOnClickListener {
+
+            // TODO Step 7: Ask for the permission while selecting the image from Gallery using Dexter Library. And Remove the toast message.
+            Dexter.withContext(this@AddUpdateDishActivity)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+
+                        // Here after all the permission are granted launch the gallery to select and image.
+                        if (report!!.areAllPermissionsGranted()) {
+                            // TODO Step 8: Show the Toast message for now just to know that we have the permission.
+                            // START
+                            Toast.makeText(
+                                this@AddUpdateDishActivity,
+                                "You have the Gallery permission now to select image.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            // END
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
+                }).onSameThread()
+                .check()
+            // END
             dialog.dismiss()
         }
 
-
-
+        //Start the dialog and display it on screen.
         dialog.show()
+    }
+
+
+    // TODO Step 5: Create a function to show the alert message that the permission is necessary to proceed further if user deny it. And ask him to allow it from setting.
+    // START
+    /**
+     * A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
+     */
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
